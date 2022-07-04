16c152105216d8eaab0923a6a9774af966508534
mihneabuz <mihneabuzatu88@gmail.com>
2022-07-05 01:13:18 +0300

    added auth service

---
+++ auth-service/cmd/api/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"auth/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const port = "3002"
+const maxTries = 16
+
+type Config struct {
+	DB *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting auth service on port", port)
+
+	conn := connectToDB()
+
+	app := Config{
+		DB: conn,
+		Models: data.New(conn),
+	}
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", port),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, err
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	log.Println(dsn)
+
+	tries := 0
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not ready yet")
+			tries += 1
+			time.Sleep(2 * time.Second)
+		} else {
+			log.Println("Connected to Postgres")
+			return connection
+		}
+
+		if tries >= maxTries {
+			log.Panic("Could not connect to Postgres")
+		}
+	}
+}
--- makefile
+++ makefile
@@ -1,11 +1,14 @@
 
-build: frontend broker
+build: frontend broker auth
 
 frontend:
 	cd frontend && env CGO_ENABLED=0 go build ./cmd/web
 
 broker:
 	cd broker-service && env CGO_ENABLED=0 go build ./cmd/api
+
+auth:
+	cd auth-service && env CGO_ENABLED=0 go build ./cmd/api
 
 start: build
 	docker-compose up --build
